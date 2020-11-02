a0e6980bce2908b1abb991ac596ba6051f460062
mertmr <mertmr@gmail.com>
2020-11-02 15:06:35 +0300

    aSD

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -21,9 +21,9 @@ const users = [
 ];
 
 const siteConfig = {
-  title: 'Test Site', // Title for your website.
-  tagline: 'A website for testing',
-  url: 'https://your-docusaurus-test-site.com', // Your website URL
+  title: 'Pirot Documentation', // Title for your website.
+  tagline: 'Pirot Documentation',
+  url: 'https://pirot-docs.netlify.app', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
@@ -31,7 +31,7 @@ const siteConfig = {
 
   // Used for publishing and more
   projectName: 'pirot',
-  organizationName: 'facebook',
+  organizationName: 'pirot',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
